e26a38be073cb0bca47ab7f0c0c8da620b65376e
FTMFR <fatemefarajzade018@gmail.com>
2024-11-24 01:13:39 +0330

    add new

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';  // Import 'path' here
+
+// This workaround is needed to get __dirname in the Vite build environment
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': dirname(fileURLToPath(import.meta.url)) + '/src',  // Path alias using __dirname workaround
     },
   },
 });
